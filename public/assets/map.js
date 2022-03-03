c5fa784735f47df8db24469033c643da6fbc7b8d
Orlando Lopez <94086814+Olopez92084@users.noreply.github.com>
2022-03-02 19:42:48 -0500

    Mapbox coding refactor 2

--- public/assets/map.js
+++ public/assets/map.js
@@ -29,47 +29,72 @@ draggable: false,
   // Add the geocoder to the map
   map.addControl(geocoder);
 
-//initiates the series of processes which run once the search is run on the webpage. If a text is entered in the input field, a search is performed
-var formSubmitHandler = function (event) {
-// prevent page from refreshing
-event.preventDefault();
+// //initiates the series of processes which run once the search is run on the webpage. If a text is entered in the input field, a search is performed
+// var formSubmitHandler = function (event) {
+// // prevent page from refreshing
+// event.preventDefault();
 
-// get value from input element
-var cityName = cityInputEl.value.trim();
-if (/[0-9]/.test(cityName)) {
-  $("#modal-error").modal("open");
-  return;
-}
+// // get value from input element
+// var cityName = cityInputEl.value.trim();
+// if (/[0-9]/.test(cityName)) {
+//   $("#modal-error").modal("open");
+//   return;
+// }
 
-if (cityName) {
-  getCityData(cityName);
-  localStoring(cityName);
+// if (cityName) {
+//   getCityData(cityName);
+//   localStoring(cityName);
 
-  cityInputEl.value = "";
-} else {
-  //$(document).ready(function () {
-  $("#modal").modal("open");
-}
-};
+//   cityInputEl.value = "";
+// } else {
+//   //$(document).ready(function () {
+//   $("#modal").modal("open");
+// }
+// };
 
-var buttonClickHandler = function (event) {
-// get the city attribute from the clicked element
-var city = event.target.getAttribute("data-city");
+// var buttonClickHandler = function (event) {
+// // get the city attribute from the clicked element
+// var city = event.target.getAttribute("data-city");
 
-if (city) {
-  getCity(city);
-}
-};
+// if (city) {
+//   getCity(city);
+// }
+// };
 
-var getCityData = function (city) {
-var city = cityInputEl.value.trim();
+// var getCityData = function (city) {
+// var city = cityInputEl.value.trim();
 
-if (city) {
-  getCity(city);
-}
-};
+// if (city) {
+//   getCity(city);
+// }
+// };
 
 //sets map to Point of Interest and plants a marker
+
+// const geojson = [
+//   {
+//     type: 'Feature',
+//     geometry: {
+//       type: 'Point',
+//       coordinates: [-77.031952, 38.913184]
+//     }
+//   },
+//   {
+//     type: 'Feature',
+//     geometry: {
+//       type: 'Point',
+//       coordinates: [-122.413682, 37.775408]
+//     }
+//   }
+// ];
+
+const mapGeo = L.mapbox.map('map_geo')
+  .setView([37.8, -96], 4)
+  .addLayer(L.mapbox.styleLayer('mapbox://styles/mapbox/light-v10'));
+
+const myLayer = L.mapbox.featureLayer().setGeoJSON(geojson).addTo(mapGeo);
+mapGeo.scrollWheelZoom.disable();
+
 map.jumpTo({
     center: [data.point.lon, data.point.lat],
     zoom: 17,
