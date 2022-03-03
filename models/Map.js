ec91dfee91a5f686d7908cf2e26c247b70c9a60f
Rhea <rheam97@gmail.com>
2022-03-03 18:34:55 -0500

    not sure about geocoder

--- models/Map.js
+++ models/Map.js
@@ -1,6 +1,7 @@
 const { Model, DataTypes, NUMBER } = require('sequelize');
 const sequelize = require('../config/connection');
 const { beforeSave } = require('./User');
+// const geocoder = require('') // need to add this with token to env if using
 
 class Map extends Model {}
 
@@ -26,7 +27,7 @@ Map.init(
         },
         formattedAddress: String,
         coordinates: {
-            type: {NUMBER}
+            type: [NUMBER]
         }
      
     },
@@ -38,31 +39,31 @@ Map.init(
       },
     }
   },
-  {
-    hooks: {
-      // node geocoder?? to turn coordinates in adddress
-      async beforeSave(newMapData) {       
-        let loc = newMapData.location
-        const lon = newMapData.lon
-        const lat = newMapData.lat
-        loc  = await geocoder.reverse({lon, lat});
-       this.location = {
-          type: 'Point',
-          formattedAddress: loc[0].formattedAddress,
-          coordinates: [loc[0].longitude, loc[0].latitude]
-        };
+//   {
+//     hooks: {
+//       // node geocoder?? to turn coordinates in adddress not sure if this is needed but in case
+//       async beforeSave(newMapData) {       
+//         let loc = newMapData.location
+//         const lon = newMapData.lon
+//         const lat = newMapData.lat
+//         loc  = await geocoder.reverse({lon, lat});
+//        this.location = {
+//           type: 'Point',
+//           formattedAddress: loc[0].formattedAddress,
+//           coordinates: [loc[0].longitude, loc[0].latitude]
+//         };
       
-        return newMapData, next()
-        // Do not save address
-        // this.address = undefined;
-      }
-    },
-    sequelize,
+//         return newMapData, next()
+//         // Do not save address
+//         // this.address = undefined;
+//       }
+//     },
+   { sequelize,
     underscored: true,
     freezeTableName: true,
     timestamps: false,
     modelName: 'map',
-  }
+}
 );
 
 module.exports = Map
