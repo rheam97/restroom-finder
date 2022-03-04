d3677418df972a57869388dcb077437554c1e353
Rhea <rheam97@gmail.com>
2022-03-03 20:38:58 -0500

    edited models

--- controllers/api/map-routes.js
+++ controllers/api/map-routes.js
@@ -3,6 +3,10 @@ const { Review, User, Bathroom, Map } = require('../../models');
 const withAuth = require('../../utils/auth.js');
 const sequelize = require('../../config/connection');
 
+
+// so each marker will need to have a get request in the popup for one bathroom
+// each bathroom will be tied to one marker by the marker id 
+
 // get all markers on map
 
 
