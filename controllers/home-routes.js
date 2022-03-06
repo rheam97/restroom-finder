d2b9ee322d5a4ad4e5df5a760b5c1ed7e7f24afb
Rhea <rheam97@gmail.com>
2022-03-06 00:28:12 -0500

    need help

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -5,44 +5,44 @@ const {Review, User, Bathroom} = require('../models')
 // may need to get fetch to geojson instead
 // non loggedin users homepage gets all markers on the map
 // get all bathrooms on map load
-// router.get('/', (req, res) => {
-//     console.log('**success')
-//       Bathroom.findAll({
-//         attributes: [
-//           'id',
-//           'title',
-//           'image_url',
-//           'lat',
-//           'lon',
-//           'gendered',
-//           'unisex',
-//           'disabled_access',
-//           'changing_tables',
-//           'key',
-//           'menstruation_products',
-//         ],
-//         include: [
-//           {
-//             model: Review,
-//             include: {
-//               model: User
-//             }
-//           },
-//           {
-//             model: User
-//           },
-//         ]
-//       }).then((dbBathroomData) => {
-//         res.json(dbBathroomData)
-//         const bathrooms = dbBathroomData.map(bathroom=> bathroom.get({plain: true}))
-//         res.render('homepage', {
-//             bathrooms,
-//             loggedIn: req.session.loggedIn})
-//   }).catch((err) => {
-//       console.log(err);
-//       res.status(500).json(err);
-//     });
-//   })
+router.get('/', (req, res) => {
+    console.log('**success')
+      Bathroom.findAll({
+        attributes: [
+          'id',
+          'title',
+          'image_url',
+          'lat',
+          'lon',
+          'gendered',
+          'unisex',
+          'disabled_access',
+          'changing_tables',
+          'key',
+          'menstruation_products',
+        ],
+        include: [
+          {
+            model: Review,
+            include: {
+              model: User
+            }
+          },
+          {
+            model: User
+          },
+        ]
+      }).then((dbBathroomData) => {
+        res.json(dbBathroomData)
+        const bathrooms = dbBathroomData.map(bathroom=> bathroom.get({plain: true}))
+        res.render('homepage', {
+            bathrooms,
+            loggedIn: req.session.loggedIn})
+  }).catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+  })
   
 // redirects loggedin user to homepage
 router.get('/login', (req, res)=> {
