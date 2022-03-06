4bb755b551756d9a9cf1c24aba83954772860af8
Rhea <rheam97@gmail.com>
2022-03-06 14:03:57 -0500

    figured out how to add map, cannot figure out why login isnt working

--- server.js
+++ server.js
@@ -26,6 +26,8 @@ const helpers = require('./utils/helpers');
 const hbs = exphbs.create({ helpers });
 
 app.engine('handlebars', hbs.engine);
+
+
 app.set('view engine', 'handlebars');
 
 app.use(express.json());
